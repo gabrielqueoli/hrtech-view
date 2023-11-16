eeb8dcc18af72d8f8e390bfc0aa48dda6984226c
Gabriel Queiroz <gabiqo12@hotmail.com>
2023-11-15 21:23:22 -0300

    Estrutura da pagina de usuario

--- hrtech-view/src/app/app.module.ts
+++ hrtech-view/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarraNavegacaoComponent } from './view/barra-navegacao/barra-navegacao.component';
+import { PerfilUsuarioComponent } from './view/perfil-usuario/perfil-usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarraNavegacaoComponent
+    BarraNavegacaoComponent,
+    PerfilUsuarioComponent
   ],
   imports: [
     BrowserModule,
---
+++ hrtech-view/src/app/view/perfil-usuario/perfil-usuario.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-usuario',
+  templateUrl: './perfil-usuario.component.html',
+  styleUrls: ['./perfil-usuario.component.css']
+})
+export class PerfilUsuarioComponent {
+}
