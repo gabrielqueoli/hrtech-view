9b0c6662c86b632da3fd8e2d8361ffd0c49cf40d
Gabriel Queiroz <gabiqo12@hotmail.com>
2023-11-18 12:40:00 -0300

    Atualização da pagina do Usuario com abas responsivas

--- hrtech-view/src/app/app.module.ts
+++ hrtech-view/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarraNavegacaoComponent } from './view/barra-navegacao/barra-navegacao.component';
 import { PerfilUsuarioComponent } from './view/perfil-usuario/perfil-usuario.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { PerfilUsuarioComponent } from './view/perfil-usuario/perfil-usuario.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hrtech-view/src/app/view/perfil-usuario/perfil-usuario.component.ts
+++ hrtech-view/src/app/view/perfil-usuario/perfil-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-perfil-usuario',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil-usuario.component.css']
 })
 export class PerfilUsuarioComponent {
+
+  perfilForm: FormGroup;
+
+  tabActive = 'beneficios';
+
+  constructor(private fb: FormBuilder) {
+    this.perfilForm = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', Validators.required],
+      senha: ['', Validators.required]
+    })
+   }
+
+  setTabActive(tab: string) {
+    this.tabActive = tab;
+  }
+
+  
 }
