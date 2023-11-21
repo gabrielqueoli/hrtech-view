7cffd709f46de125c3a7d3fc38691fde68c4ce8b
xDraagon <xdraagon@gmail.com>
2023-11-20 23:29:04 -0300

    Alterações

--- hrtech-view/src/app/app-routing.module.ts
+++ hrtech-view/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 import { PaginaEntradaComponent } from './view/pagina-entrada/pagina-entrada.component';
 import { BarraNavegacaoComponent } from './view/barra-navegacao/barra-navegacao.component';
+import { PaginaHoleriteComponent } from './view/pagina-holerite/pagina-holerite.component';
+import { PerfilUsuarioComponent } from './view/perfil-usuario/perfil-usuario.component';
 
 const routes: Routes = [
   { path: 'paginaentrada', component: PaginaEntradaComponent },
-  {path: 'barranavegacao', component: BarraNavegacaoComponent}
+  {path: 'barranavegacao', component: BarraNavegacaoComponent},
+  {path: 'holerite', component: PaginaHoleriteComponent},
+  {path: 'perfilusuario', component: PerfilUsuarioComponent}
+  
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
